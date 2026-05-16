b3d75ffcbd1190f0badfa6730ff9fcec0d71d5c9
mpinchover <mpinchover@gmail.com>
2026-05-16 11:54:47 -0700

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Meditate Now",
+  title: "Meditation before bed.",
   description: "Configure your meditation session",
 };
 
